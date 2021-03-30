e915f3969ac45c50d6bbbfd7b443cd0dcf8653f1
cmtudor <31376942+chancetudor@users.noreply.github.com>
2021-03-29 20:27:02 -0400

    search functionality being implemented. working on pulling data from each result

--- cmd/search.go
+++ cmd/search.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/anaskhan96/soup"
 	"github.com/spf13/cobra"
+	"log"
 	"net/url"
 	"strings"
 )
@@ -11,13 +13,14 @@ var Site string
 
 func init() {
 	rootCmd.AddCommand(searchCmd)
-
+	// flags to fine-tune search command
+	// each flag corresponds to standard DDG search syntax
 	searchCmd.Flags().BoolP("exact", "e", false, "results for exact query, e.g. cats --exact")
-	searchCmd.Flags().StringVarP(&Site, "site", "s", "", "site to query directly, e.g. cats --site aspca.org")
 	searchCmd.Flags().BoolP("title", "t", false, "page title includes the query, e.g. cats --title")
 	searchCmd.Flags().BoolP("url", "u", false, "page URL includes the query, e.g. cats --url")
 	searchCmd.Flags().BoolP("news", "n", false, "returns news about the query, e.g. cincinnati --news")
 	searchCmd.Flags().BoolP("map", "m", false, "returns map results about the query, e.g. cincinnati --map")
+	searchCmd.Flags().StringVarP(&Site, "site", "s", "", "site to query directly, e.g. cats --site aspca.org")
 }
 
 // searchCmd represents the search command
@@ -29,18 +32,20 @@ var searchCmd = &cobra.Command{
 To visit a link, simply press the number corresponding with the link.
 There are a number of flags available to fine-tune search results.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		exactSwitch, _ := cmd.Flags().GetBool("exact")
-		siteSwitch, _ := cmd.Flags().GetString("site")
-		titleSwitch, _ := cmd.Flags().GetBool("title")
-		urlSwitch, _ := cmd.Flags().GetBool("url")
-		newsSwitch, _ := cmd.Flags().GetBool("news")
-		mapSwitch, _ := cmd.Flags().GetBool("map")
+		/*
+		exactSwitch, _ 		:= cmd.Flags().GetBool("exact")
+		siteSwitch, _ 		:= cmd.Flags().GetString("site")
+		titleSwitch, _ 		:= cmd.Flags().GetBool("title")
+		urlSwitch, _ 		:= cmd.Flags().GetBool("url")
+		newsSwitch, _ 		:= cmd.Flags().GetBool("news")
+		mapSwitch, _ 		:= cmd.Flags().GetBool("map")
+		*/
+		var query string
 
+		/*
 		switch {
 		case exactSwitch:
 			fmt.Println("exact")
-		case siteSwitch != "":
-			fmt.Println(siteSwitch)
 		case titleSwitch:
 			fmt.Println("title")
 		case urlSwitch:
@@ -49,15 +54,36 @@ There are a number of flags available to fine-tune search results.`,
 			fmt.Println("news")
 		case mapSwitch:
 			fmt.Println("map")
+		case siteSwitch != "":
+				fmt.Println(Site)
 		}
-		query := parseQuery(args)
-		url := generateURL(query)
+		*/
+
+		query 				= parseQuery(args)
+		url 				:= generateURL(query)
 		fmt.Println(url.String())
+		results 			:= search(url)
+		printResults(results)
 	},
 }
 
-func search(query string) {
+func printResults(results []soup.Root) {
+	for i := 0; i < len(results); i = i + 1 {
+		fmt.Printf("[%d] ", i)
+		fmt.Println(results[i].Text(), " :", results[i].Attrs()["href"])
+	}
+}
 
+func search(url *url.URL) []soup.Root {
+	resp, err 				:= soup.Get(url.String())
+	if err != nil {
+		fmt.Println("An error has occurred fetching the results. Exiting.")
+		log.Fatal(err)
+	}
+	doc 					:= soup.HTMLParse(resp)
+	results 				:= doc.FindAll("div", "class", "links_main links_deep result__body")
+
+	return results
 }
 
 /* TODO complete
@@ -79,11 +105,11 @@ func parseQuery(args []string) string {
 }
 
 func generateURL(query string) *url.URL {
-	baseUrl, _ := url.Parse("https://html.duckduckgo.com")
+	baseUrl, _ 				:= url.Parse("https://html.duckduckgo.com")
 	baseUrl.Path += "/html"
-	params := url.Values{}
+	params 					:= url.Values{}
 	params.Add("q", query)
-	baseUrl.RawQuery = params.Encode()
+	baseUrl.RawQuery 		= params.Encode()
 
 	return baseUrl
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module duck
 go 1.16
 
 require (
+	github.com/anaskhan96/soup v1.2.4 // indirect
 	github.com/enescakir/emoji v1.0.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.1.3
